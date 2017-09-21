d233855bfee9dbdb298923b0c1973ad0b970387d
Kanelchefen <elias.rhoden@hotmail.com>
2017-09-21 14:28:50 +0200

    Basic graphics fungerar

--- graphics.js
+++ graphics.js
@@ -2,19 +2,25 @@ const numberofBlocksInX = 10;
 const numberOfBlocksInY = 22;
 const playFieldHeightPrecentage = 75;
 var blockWidht = 40;        //Pixels
+var canvasID = "";
 
-function createGraphics(){
+
+
+function createGraphics(_canvasID){
+    canvasID = _canvasID;
     blockWidht = (screen.height * playFieldHeightPrecentage/100)/numberOfBlocksInY; 
-    
-    document.body.innerHTML += "<canvas id=\"mainCanvas\" widht=\"" + 
-    blockWidht*numberofBlocksInX +
-    "\" height=\""+ blockWidht*numberOfBlocksInY +
-    "\" style=\"border:1px solid #000000;\"></canvas>";
-
+    document.getElementById(canvasID).clientWidth = blockWidht*numberofBlocksInX;
+    document.getElementById(canvasID).height = blockWidht*numberOfBlocksInY; 
 }
 
 function drawBlock(x,y,color){
-    
+
+    var c=document.getElementById(canvasID);
+    var ctx=c.getContext("2d"); 
+    ctx.fillStyle = color;
+    ctx.fillRect(x*blockWidht,y*blockWidht,blockWidht,blockWidht);
+    //ctx.strokeRect(x*blockWidht,y*blockWidht,blockWidht,blockWidht);
+
 }
 
 
