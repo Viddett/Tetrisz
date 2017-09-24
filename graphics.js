e06e6aff8b84672d70f0d91d4ead695fdbe60703
Elias R <elias.rhoden@hotmail.com>
2017-09-24 10:53:18 +0200

    Tog bort lite sheit

--- graphics.js
+++ graphics.js
@@ -1,4 +1,4 @@
-/* Handles the graphics for the tetris-game, to use: insert a 'canvas' 
+/* Handles the graphics for the tetris-game, to use: insert a 'canvas'
 in the HTML file with an ID and use the createGraphics()
 */
 
@@ -8,20 +8,20 @@ const numberOfBlocksInY = 22;
 const playFieldHeightPrecentage = 75;
 
 //Variables
-var blockWidht = 40;       
+var blockWidht = 40;
 var canvasID = "";
 
 function createGraphics(_canvasID){
     canvasID = _canvasID;
-    blockWidht = (screen.height * playFieldHeightPrecentage/100)/numberOfBlocksInY; 
+    blockWidht = (screen.height * playFieldHeightPrecentage/100)/numberOfBlocksInY;
     document.getElementById(canvasID).clientWidth = blockWidht*numberofBlocksInX;
-    document.getElementById(canvasID).height = blockWidht*numberOfBlocksInY; 
+    document.getElementById(canvasID).height = blockWidht*numberOfBlocksInY;
 }
 // Coords are in blocks, NOT pixels!
 function drawBlock(x,y,color){
     if (valid_X(x) && valid_Y(y)){
         var c=document.getElementById(canvasID);
-        var ctx=c.getContext("2d"); 
+        var ctx=c.getContext("2d");
         ctx.fillStyle = color;
         ctx.fillRect(x*blockWidht,y*blockWidht,blockWidht,blockWidht);
         ctx.strokeStyle = 0xFFF;
@@ -31,8 +31,6 @@ function drawBlock(x,y,color){
     }
 }
 
-<<<<<<< HEAD
-=======
 function valid_X(x){
     return x >= 0 && x < numberofBlocksInX;
 }
@@ -40,4 +38,3 @@ function valid_X(x){
 function valid_Y(y){
     return y >= 0 && y < numberOfBlocksInY;
 }
->>>>>>> EliasDevAndSwag
